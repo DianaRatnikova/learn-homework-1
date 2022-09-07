821b650f5283b8b0852f9e9a2d1fb871a41a1cf8
DianaRatnikova <Diana_r@bk.ru>
2022-09-07 23:55:45 +0300

    Put exceptions in order

--- 7_exception2.py
+++ 7_exception2.py
@@ -14,27 +14,31 @@
 """
 
 
+
 def discounted(price, discount, max_discount=20):
     try:
-        price=float(price)
-        discount=float(discount)
-        max_discount=int(max_discount)
-        if max_discount >=100:
-            raise ValueError("Слишком большая максимальная скидка")
-        elif discount>=max_discount:
-            return price
-        else:
-            return price-(price*discount/100)
+        price_float = float(price)
+        discount_float = float(discount)
+        max_discount_float = int(max_discount)
     except (TypeError, ValueError):
         print("Ошибка типов")
         return "Err"
 
+    if max_discount_float >= 100:
+        raise ValueError("Слишком большая максимальная скидка")
+    if discount_float >= max_discount_float:
+        final_price = price_float*(1-max_discount_float/100)
+    else:
+        final_price = price_float*(1-discount_float/100)
+    return final_price
 
 
 if __name__ == "__main__":
+    print("7.", discounted(100.0, 5, 1000))
     print("1.", discounted(100, 2))
     print("2.", discounted(100, "3"))
     print("3.", discounted("100", "4.5"))
     print("4.", discounted("five", 5))
     print("5.", discounted("сто", "десять"))
     print("6.", discounted(100.0, 5, "10"))
+    print("7.", discounted(100.0, 5, 1000))
