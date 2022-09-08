e7273438b89f6c6935bcd83921061e341d3a3945
DianaRatnikova <Diana_r@bk.ru>
2022-09-08 22:25:49 +0300

    Considered absence of data

--- 3_for.py
+++ 3_for.py
@@ -1,3 +1,4 @@
+
 """
 
 Домашнее задание №1
@@ -15,45 +16,59 @@
 * Посчитать и вывести суммарное количество продаж всех товаров  +
 * Посчитать и вывести среднее количество продаж всех товаров    +
 """
-
-phones_sold=  [
+#phones_sold_etalon
+phones_sold =  [
     {'product': 'iPhone 12', 'items_sold': [363, 500, 224, 358, 480, 476, 470, 216, 270, 388, 312, 186]},
     {'product': 'Xiaomi Mi11', 'items_sold': [317, 267, 290, 431, 211, 354, 276, 526, 141, 453, 510, 316]},
     {'product': 'Samsung Galaxy 21', 'items_sold': [343, 390, 238, 437, 214, 494, 441, 518, 212, 288, 272, 247]},
   ]
 
+'''
+Словари для тестирования.
+Надо ли их сохранять на случай дальнейшей доработки проги?
+'''
+phones_sold_nodata = []
 
-# суммарноеколичество продаж для каждого товара
-def total(items_sold):
-    return sum(items_sold)
+phones_sold_no_sold =  [
+    {'product': 'iPhone 12', 'items_sold': []},
+    {'product': 'Xiaomi Mi11', 'items_sold': []},
+    {'product': 'Samsung Galaxy 21', 'items_sold': []},
+  ]
+
 
 # среднее количество продаж для каждого товара
 def avg(items_sold):
-    return sum(items_sold)/len(items_sold)
+    sold_in_average=0
+    sold_in_average = sum(items_sold)/len(items_sold)
+    return sold_in_average
 
 
 def print_phones(phones_sold):
     for phone in phones_sold:
-        print('product:      ', phone['product'])
-        print('sold_total:   ', phone['sold_total'])
-        print(f"sold_average:  {sold_average:.6f}")
+        print(f"product:      {phone['product']}" )
+        print(f"sold_total:   {phone['sold_total']}")
+        print(f"sold_average: {phone['sold_average']:.6}")
         print("------------------------")
 
 
 def main():
-    sale_for_all_total=0
-    sale_for_all_avg=0
-    for phone in phones_sold:
-        phone['sold_total']=total(phone['items_sold'])
-        phone['sold_average']=avg(phone['items_sold'])
-        sale_for_all_total+=phone['sold_total']
-        sale_for_all_avg+=phone['sold_average']
-    sale_for_all_avg=sale_for_all_avg/len(phones_sold)
-    print_phones(phones_sold)
-    print(f"SOLD IN TOTAL:   {sale_for_all_total}")
-    print(f"SOLD IN AVERAGE: {sale_for_all_avg}")
-
-    pass
+    sale_for_all_total = 0
+    sale_avg_onephone = 0
+    if not phones_sold:              #если весь словарь пустой
+        print('Нет данных о товарах')
+    else:
+        for phone in phones_sold:
+            if not phone.get('items_sold'):
+                print(f"{phone['product']}:  Нет данных о продажах") # указать название телефона
+            else:
+                phone['sold_total'] = sum(phone['items_sold'])  
+                phone['sold_average'] = avg(phone['items_sold'])
+                sale_for_all_total += phone['sold_total']
+        if phone.get('items_sold'):
+            print_phones(phones_sold)    
+            sale_for_all_avg = sum(avg['sold_average'] for avg in phones_sold)/len(phones_sold)
+            print(f"SOLD IN TOTAL:   {sale_for_all_total}")
+            print(f"SOLD IN AVERAGE: {sale_for_all_avg}")
     
 if __name__ == "__main__":
     main()
